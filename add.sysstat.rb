72e5692f7979ea1422c0e0007fb5fe888ae46ca2
stephenkeane76 <x00081541@ittd.ie>
2014-11-21 11:48:03 +0000

    commit

---
+++ add.sysstat.rb
@@ -0,0 +1,8 @@
+package 'sysstat' do
+action :install
+end
+
+log 'message' do
+message "sysstat Package installed!."
+level :info
+end
---
+++ add.tdi.file.rb
@@ -0,0 +1,5 @@
+file 'file/tdi/user_readme' do
+action :create
+content 'http_proxy=http://localhost:3128 export 
+         GIT_SSL_NO_VERIFY=1'
+end
